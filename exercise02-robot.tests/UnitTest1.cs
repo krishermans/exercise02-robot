82dbe4602299b94900e563af314ff429f71b2fd3
Kris Hermans <Kris.Hermans@live.be>
2022-10-14 13:27:25 +0200

    add project files

---
+++ exercise02-robot.tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+
+namespace exercise02_robot.tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Fail("Nothing written yet!");
+    }
+
+    [Test]
+
+    public void Test2()
+    {
+        Assert.Pass();
+    }
+}
